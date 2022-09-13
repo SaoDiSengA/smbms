c8b426befa7521b8df58793a4fefbccb338ec20b
SaoDiSeng <1300808192@qq.com>
2022-09-13 23:57:50 +0800

    basedao，filter

--- src/main/java/dao/BaseDao.java
+++ src/main/java/dao/BaseDao.java
@@ -2,18 +2,16 @@ package dao;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 //操作数据库的公共类
 public class BaseDao {
 
-    private static String url;
-    private static String username;
-    private static String password;
-    private static String driver;
+    private static final String url;
+    private static final String username;
+    private static final String password;
+    private static final String driver;
 
     //静态代码快，类加载就会执行
     static {
@@ -46,5 +44,56 @@ public class BaseDao {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    //编写查询公共方法
+    public static ResultSet executeQuery(Connection connection,String sql,Object[] objects) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i < objects.length; i++) {
+            preparedStatement.setObject(i+1,objects[i]);
+        }
+        return preparedStatement.executeQuery();
+    }
+
+    //编写曾删改公共方法
+    public static int executeUpdate(Connection connection,String sql,Object[] objects) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i < objects.length; i++) {
+            preparedStatement.setObject(i+1,objects[i]);
+        }
+        return preparedStatement.executeUpdate();
+    }
+
+    //关闭链接 释放资源
+    public static boolean closeResources(Connection connection,PreparedStatement preparedStatement,ResultSet resultSet){
+        boolean flag = true;
+        if (resultSet!=null){
+            try {
+                resultSet.close();
+                //提示GC回收 resultSet = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                flag = false;
+            }
+        }
+        if (preparedStatement!=null){
+            try {
+                preparedStatement.close();
+                //提示GC回收 preparedStatement = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                flag = false;
+            }
+        }
+        if (connection!=null){
+            try {
+                connection.close();
+                //提示GC回收 connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                flag = false;
+            }
+        }
+        return flag;
     }
 }
